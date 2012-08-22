af1e0e57d782dab3a5cf081c77596bcd0ddd1e57
Kacper Kozak <dexted.xt@gmail.com>
2012-08-22 11:39:19 +0200

    0.3

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Valider
 Walidator formularzy w HTML5.
 
+## License
+Valider is open-sourced software licensed under the MIT License.
+
 ## Changelog
+
+### 0.3
+- Usunięto Lo-dash
+- Dodano wymuszenie typu inputa za pomocą data-type
+- Dodana walidacja URLi
+- Strict mode
+- Lepszy data-equals
+
 ### 0.2
 - Porównanie dwóch inputów (data-equals)
 - Tłumaczenie na angielski
--- js/valider.js
+++ js/valider.js
@@ -1,13 +1,21 @@
+/*
+ * Valider - HTML5 Form Validation
+ * @author DeXTeD
+ * @version 0.3
+ * @license MIT
+ */
+
 (function($){
+	"use strict";
 
 	// Add HTML5 input types
-	$.each(["search", "tel", "url", "email", "datetime", "date", "month", "week", "time", "datetime-local", "number", "range", "color"], function(i, t) {
+	$.each(["tel", "url", "email", "datetime", "date", "month", "week", "time", "datetime-local", "number", "range", "color"], function(i, t) {
 		$.expr[":"][t] = function(elem){
 			return elem.getAttribute("type") === t;
 		};
 	});
 
-	window.Valider = function(form, config) {
+	var Valider = function(form, config) {
 
 		var self = this;
 
@@ -15,11 +23,12 @@
 		this.config = $.extend({
 			onInputError: function(error) {},
 			onErrors: function(errors) {
+				// Simple error
 				var arr = [];
 				$.each(errors, function(key, value) {
 					arr.push(value);
 				});
-				alert(arr.join(', '));
+				alert(arr.join('\n'));
 			},
 			onInputPass: function() {},
 			lang: $('html').attr('lang') || 'en'
@@ -32,7 +41,7 @@
 		this.form = form;
 		this.inputs = form.find(':input:not(:button, :image, :reset, :submit, :disabled), textarea:not(:disabled)');
 
-		// Validate on submit
+		// Validate on submit and remove native HTML5 form validation
 		form.on('submit.valider', function(event) {
 			self.validate(self.inputs, event);
 		}).attr('novalidate', 'novalidate');
@@ -44,15 +53,12 @@
 	Valider.prototype = {
 
 		regex: {
-			'zipcode': /^\d+[0-9\-]+\d+?$/,
-			'name': /^[a-zA-Z -\/.,0-9]*$/,
-			'address': /^([0-9])+([0-9a-zA-Z -\/.,#])*$/,
-			'letters': /^[a-zA-Z]*$/,
+			'number': /^-?[0-9]*(\.[0-9]+)?$/,
+			// 'zipcode': /^\d+[0-9\-]+\d+?$/,
+			// 'letters': /^[a-zA-Z]*$/,
+			// 'city': /^[A-Za-z. '-]+$/,
 			'email': /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/,
-			'url': /https?:\/\/([_a-z\d\-]+(\.[_a-z\d\-]+)+)(([_a-z\d\-\\\.\/]+[_a-z\d\-\\\/])+)*/,
-			'city': /^[A-Za-z. '-]+$/,
-			'password': /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-			'credit-card': /^([0-9]{2})+\/+([0-9]{2})$/
+			'url': /https?:\/\/([_a-z\d\-]+(\.[_a-z\d\-]+)+)(([_a-z\d\-\\\.\/]+[_a-z\d\-\\\/])+)*/
 		},
 
 		errors: {
@@ -84,7 +90,7 @@
 			var error = '',
 				attrs = {
 					'name': input.data('name') || input.attr('name'),
-					'val': _.escape(input.val()),
+					'val': input.val(),
 					'min': input.attr("min"),
 					'equals': input.data("equalsName"),
 					'max': input.attr("max")
@@ -98,9 +104,17 @@
 				error = this.errors[this.config.lang][key];
 			}
 
-			// No error? Get default
-			if(_.isUndefined(error)) {
-				error = this.errors[this.config.lang]['*'];
+			// No error?
+			if(typeof error === 'undefined') {
+				// data type
+				var type = input.data('type');
+				if(type) {
+					error = this.errors[this.config.lang][':'+type];
+				}
+				// or default
+				if(typeof error === 'undefined') {
+					error = this.errors[this.config.lang]['*'];
+				}
 			}
 
 			// Replace all attributes
@@ -133,25 +147,28 @@
 					// TODO radio support
 					return true;
 				}
-				return !_.isEmpty(val);
+				return !!val;
 			},
-			':email': function(input, val) {
-				return val==='' || this.regex.email.test(val);
+			':email, [data-type="email"]': function(input, val) {
+				return val === '' || this.regex.email.test(val);
 			},
-			':number': function(input, val) {
-				return val==='' || _.isFinite(parseInt(val, 10));
+			':number, [data-type="number"]': function(input, val) {
+				return val === '' || this.regex.number.test(val);
+			},
+			':url, [data-type="url"]': function(input, val) {
+				return val === '' || this.regex.url.test(val);
 			},
 			'[max]': function(input, val) {
-				return val==='' || (parseFloat(val) <= parseFloat(input.attr("max"))) ? true : false;
+				return val === '' || (parseFloat(val) <= parseFloat(input.attr("max")));
 			},
 			'[min]': function(input, val) {
-				return val==='' || (parseFloat(val) >= parseFloat(input.attr("min"))) ? true : false;
+				return val === '' || (parseFloat(val) >= parseFloat(input.attr("min")));
 			},
 			'[pattern]': function(input, val) {
 				return val === '' || new RegExp("^" + input.attr("pattern") + "$").test(val);
 			},
 			'[data-equals]': function(input, val) {
-				var input2 = this.form.find('[name='+input.data('equals')+']');
+				var input2 = this.inputs.filter('[name='+input.data('equals')+']');
 				return val === input2.val();
 			}
 		},
@@ -177,22 +194,30 @@
 
 		// Bind input to recheck value on change
 		bindInput: function(input) {
-			var self = this;
+			var self = this,
+				timer;
 			// Remove all to prevent multiple binds
 			this.unBindInput(input);
 			// Bind all possible events and debounce them
-			input.on('keyup.valider-input change.valider-input click.valider-input', _.debounce(function(event) {
-				var check = self.validateInput(input);
-				if(self.validateInput(input) === true) {
-					delete self.incorrectInputs[input.attr('name')];
-					self.callInputPass(input);
-					self.unBindInput(input);
-				} else {
-					// Save error name
-					self.incorrectInputs[input.attr('name')] = check;
-				}
-			}, 200));
+			input.on('keyup.valider-input change.valider-input click.valider-input', function() {
+				clearTimeout(timer);
+				timer = setTimeout(function() {
+					var check = self.validateInput(input);
+					if(self.validateInput(input) === true) {
+						// Delete error
+						delete self.incorrectInputs[input.attr('name')];
+						// user callback
+						self.callInputPass(input);
+						// Unbind recheck
+						self.unBindInput(input);
+					} else {
+						// Save error name
+						self.incorrectInputs[input.attr('name')] = check;
+					}
+				}, 100);
+			});
 		},
+
 		// Unbind input to re-check value on change
 		unBindInput: function(input) {
 			input.off('.valider-input');
